fb9c04a9fb24b123828fbd1641d42a3b5d4560d7
Junaid Siddiqui <maxeinstein.dev@gmail.com>
2022-02-19 11:55:00 +0530

    Removed MLPC and SVM models becoz of their large sizes

--- backend/apps/checker/views.py
+++ backend/apps/checker/views.py
@@ -15,12 +15,12 @@ def news_checker(request):
     if((url is not None) and (len(url) > 5)):
         # 1. Load the model from disk
         print("Setting up")
-        svc_model = pickle.load(
-            open('E:/Highway/thecodejournalists/backend/apps/checker/svc_model.sav', 'rb'))
-        mlp_model = pickle.load(open(
-            'E:/Highway/thecodejournalists/backend/apps/checker/MLPC_model.sav', 'rb'))
+        # svc_model = pickle.load(
+        #     open('E:/Highway/thecodejournalists/backend/apps/checker/svc_model.sav', 'rb'))
+        # mlp_model = pickle.load(open(
+        #     'E:/Highway/thecodejournalists/backend/apps/checker/MLPC_model.sav', 'rb'))
         log_model = pickle.load(
-            open('E:/Highway/thecodejournalists/backend/apps/checker/log_model.sav', 'rb'))
+            open('E:/Deployed/thecodejournalists/backend/apps/checker/log_model.sav', 'rb'))
         cDict = loadCanonDict()
         ss = SoupStrainer()
         ss.init()
@@ -37,11 +37,11 @@ def news_checker(request):
         print('NOW IT IS BELOW articleX')
 
         # 5. Send the X row to the model to produce a prediction
-        svc_prediction = svc_model.predict(articleX)
-        svc_probabilities = svc_model.predict_proba(articleX)
+        # svc_prediction = svc_model.predict(articleX)
+        # svc_probabilities = svc_model.predict_proba(articleX)
 
-        mlp_prediction = mlp_model.predict(articleX)
-        mlp_probabilities = mlp_model.predict_proba(articleX)
+        # mlp_prediction = mlp_model.predict(articleX)
+        # mlp_probabilities = mlp_model.predict_proba(articleX)
 
         log_prediction = log_model.predict(articleX)
         log_probabilities = log_model.predict_proba(articleX)
@@ -51,19 +51,19 @@ def news_checker(request):
         # with a ruling of yes or no, is it real or not. Then we'll give the probabiilties to allow
         # the user to make up their mind.
 
-        svc_prb = (svc_probabilities[0][0]*100, svc_probabilities[0][1]
-                   * 100, svc_probabilities[0][2]*100, svc_probabilities[0][3]*100)
-        svc_totFake = (svc_probabilities[0][0]
-                       * 100) + (svc_probabilities[0][1]*100)
-        svc_totReal = (svc_probabilities[0][2]
-                       * 100) + (svc_probabilities[0][3]*100)
+        # svc_prb = (svc_probabilities[0][0]*100, svc_probabilities[0][1]
+        #            * 100, svc_probabilities[0][2]*100, svc_probabilities[0][3]*100)
+        # svc_totFake = (svc_probabilities[0][0]
+        #                * 100) + (svc_probabilities[0][1]*100)
+        # svc_totReal = (svc_probabilities[0][2]
+        #                * 100) + (svc_probabilities[0][3]*100)
 
-        mlp_prb = (mlp_probabilities[0][0]*100, mlp_probabilities[0][1]
-                   * 100, mlp_probabilities[0][2]*100, mlp_probabilities[0][3]*100)
-        mlp_totFake = (mlp_probabilities[0][0]
-                       * 100) + (mlp_probabilities[0][1]*100)
-        mlp_totReal = (mlp_probabilities[0][2]
-                       * 100) + (mlp_probabilities[0][3]*100)
+        # mlp_prb = (mlp_probabilities[0][0]*100, mlp_probabilities[0][1]
+        #            * 100, mlp_probabilities[0][2]*100, mlp_probabilities[0][3]*100)
+        # mlp_totFake = (mlp_probabilities[0][0]
+        #                * 100) + (mlp_probabilities[0][1]*100)
+        # mlp_totReal = (mlp_probabilities[0][2]
+        #                * 100) + (mlp_probabilities[0][3]*100)
 
         log_prb = (log_probabilities[0][0]*100, log_probabilities[0][1]
                    * 100, log_probabilities[0][2]*100, log_probabilities[0][3]*100)
@@ -72,14 +72,17 @@ def news_checker(request):
         log_totReal = (log_probabilities[0][2]
                        * 100) + (log_probabilities[0][3]*100)
 
-        fin_prb = ((((svc_probabilities[0][0]*100)+(mlp_probabilities[0][0]*100)+(log_probabilities[0][0]*100))/3),
-                   (((svc_probabilities[0][1]*100)+(mlp_probabilities[0]
-                                                    [1]*100)+(log_probabilities[0][1]*100))/3),
-                   (((svc_probabilities[0][2]*100)+(mlp_probabilities[0]
-                                                    [2]*100)+(log_probabilities[0][2]*100))/3),
-                   (((svc_probabilities[0][3]*100)+(mlp_probabilities[0][3]*100)+(log_probabilities[0][3]*100))/3))
-        fin_totFake = (svc_totFake + mlp_totFake + log_totFake)/3
-        fin_totReal = (svc_totReal + mlp_totReal + log_totReal)/3
+        # fin_prb = ((((svc_probabilities[0][0]*100)+(mlp_probabilities[0][0]*100)+(log_probabilities[0][0]*100))/3),
+        #            (((svc_probabilities[0][1]*100)+(mlp_probabilities[0]
+        #                                             [1]*100)+(log_probabilities[0][1]*100))/3),
+        #            (((svc_probabilities[0][2]*100)+(mlp_probabilities[0]
+        #                                             [2]*100)+(log_probabilities[0][2]*100))/3),
+        #            (((svc_probabilities[0][3]*100)+(mlp_probabilities[0][3]*100)+(log_probabilities[0][3]*100))/3))
+        # fin_totFake = (svc_totFake + mlp_totFake + log_totFake)/3
+        # fin_totReal = (svc_totReal + mlp_totReal + log_totReal)/3
+        fin_prb = log_prb
+        fin_totFake = log_totFake
+        fin_totReal = log_totReal
 
         print('NOW IT IS BELOW fin')
 
@@ -97,7 +100,7 @@ def news_checker(request):
                                                 '_').replace(':', '_').replace('-', '_') + ".svg"
         path = "/static/images/wordclouds/" + imgname
 
-        with open(f'E:/Highway/thecodejournalists/frontend/assets/images/wordclouds/{imgname}', 'wb') as f:
+        with open(f'E:/Deployed/thecodejournalists/frontend/assets/images/wordclouds/{imgname}', 'wb') as f:
             f.write(resp.content)
 
         url = "https://bing-news-search1.p.rapidapi.com/news/search"
@@ -138,14 +141,14 @@ def news_checker(request):
         context = {'headline': ss.recHeadline, 'words': ss.extractText,
                    'url': url, 'cloud':  path,
                    'articles': articles_list,
-                   'svc_totFake': svc_totFake,
-                   'svc_totReal': svc_totReal,
-                   'svc_prediction': svc_prediction.tolist(),
-                   'svc_probabilities': svc_prb,
-                   'mlp_totFake': mlp_totFake,
-                   'mlp_totReal': mlp_totReal,
-                   'mlp_prediction': mlp_prediction.tolist(),
-                   'mlp_probabilities': mlp_prb,
+                   #    'svc_totFake': svc_totFake,
+                   #    'svc_totReal': svc_totReal,
+                   #    'svc_prediction': svc_prediction.tolist(),
+                   #    'svc_probabilities': svc_prb,
+                   #    'mlp_totFake': mlp_totFake,
+                   #    'mlp_totReal': mlp_totReal,
+                   #    'mlp_prediction': mlp_prediction.tolist(),
+                   #    'mlp_probabilities': mlp_prb,
                    'log_totFake': log_totFake,
                    'log_totReal': log_totReal,
                    'log_prediction': log_prediction.tolist(),
